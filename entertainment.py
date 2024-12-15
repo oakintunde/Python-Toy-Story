833135c87d591cc5ead6f19fdbffea278b1c6755
oakintunde <144389474+oakintunde@users.noreply.github.com>
2024-12-15 01:25:56 -0800

    Add ratings function

--- entertainment.py
+++ entertainment.py
@@ -4,30 +4,31 @@ import media
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+                        "https://youtu.be/Ov-oqVKokBo")
 #print(toy_story.storyline)
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "http://www.youtube.com/watch?v=-9ceBgWV8io")
+                     "https://youtu.be/cZHaBYb7Q2c")
 #print(avatar.storyline)
 #avatar.show_trailer()
 school_of_work = media.Movie("School of work",
                      "A marine on an alien planet",
                      "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
-                     "http://www.youtube.com/watch?v=-3PsUJFEBC74")
+                     "https://youtu.be/Pbo5cDMmscY")
 ratatouille = media.Movie("Ratatouille",
                      "A storyline on an alien planet",
                      "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
-                     "http://www.youtube.com/watch?v=-c3sBBRxDAgk")
+                     "https://youtu.be/lrdmnAn9gxk")
 midnight_in_paris = media.Movie("Midnight in paris",
                      "A marine on an alien planet",
                      "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
-                     "http://www.youtube.com/watch?v=-atLg2wQQxvU")
+                     "https://youtu.be/K4H7n4Y_M68")
 hunger_games = media.Movie("Hunger Games",
                      "A Hunger Games on an alien planet",
                      "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
-                     "http://www.youtube.com/watch?v=-PbA63a7H0bo")
+                     "https://youtu.be/ZPou6asM4II")
 
 movies = [toy_story, avatar, school_of_work, ratatouille, midnight_in_paris, hunger_games]
 fresh_tomatoes.open_movies_page(movies)
+print(media.Movie.valid_ratings)
