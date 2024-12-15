1ef89310325ed6c80254c58c9a1adfdc0aee35a6
oakintunde <144389474+oakintunde@users.noreply.github.com>
2024-12-15 05:17:52 -0800

    Create inheritance file

---
+++ inheritance.py
@@ -0,0 +1,16 @@
+class Parent():
+    def __init__(self, last_name, eye_color):
+        print("Parent constructor called")
+        self.last_name = last_name
+        self.eye_color = eye_color
+
+    def Child(Parent):
+        def __init__(self, last_name, eye_color, number_of_toys):
+            print("Child constructor called")
+            Parent.__init__(self, last_name, eye_color)
+            self.number_of_toys = number_of_toys
+
+            miley_cyrus = Child("Cyrus", "Blue", 5)
+            print(miley_cyrus.last_name)
+            print(miley_cyrus.number_of_toys)
+    
