da44344428b88012846f8b32d1951b445593ad80
oakintunde <144389474+oakintunde@users.noreply.github.com>
2024-12-15 05:26:37 -0800

    Reuse method

--- inheritance.py
+++ inheritance.py
@@ -4,13 +4,17 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+   def show_info(self):
+    print("Last name - "+self.last_name)
+    print("Eye Color - "+self.eye_color)
+
     def Child(Parent):
         def __init__(self, last_name, eye_color, number_of_toys):
             print("Child constructor called")
             Parent.__init__(self, last_name, eye_color)
             self.number_of_toys = number_of_toys
 
+            billy_cyrus = Child("Cyrus", "Blue")
+            billy_cyrus.show_info()
             miley_cyrus = Child("Cyrus", "Blue", 5)
-            print(miley_cyrus.last_name)
-            print(miley_cyrus.number_of_toys)
     
